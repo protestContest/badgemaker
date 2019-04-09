071e3303f440d973053fe54a2fb1227bb9effcaa
Zack Michener <zackm@zillowgroup.com>
2019-04-09 10:46:38 -0700

    add prod build

---
+++ webpack.prod.config.js
@@ -0,0 +1,46 @@
+let webpack = require('webpack');
+let path = require('path');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    filename: 'dist/app.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.woff2?$/,
+        loader: 'base64-font-loader'
+      },
+      {
+        test: /\.png?$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  watch: false,
+  plugins: [
+    new CleanWebpackPlugin('./dist'),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        context: process.cwd()
+      }
+    })
+  ]
+};
